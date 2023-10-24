ddf83e7462f04c0d1208409b8870fb9080295d46
Gustavo Carvalho <gustavo.hcarvalho@outlook.com>
2023-10-24 17:07:23 -0300

    feat: use attack roll, damage roll and target ac for damage

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings, NamedFieldPuns, TypeApplications, LambdaCase #-}
+{-# LANGUAGE OverloadedStrings, NamedFieldPuns, LambdaCase #-}
 
 module Main where
 
@@ -8,22 +8,32 @@ import Control.Monad ( join )
 import System.Random.Stateful ( uniformRM, globalStdGen )
 import Data.List.NonEmpty
 
--- AC
--- Roll > AC
--- Roll for damage
--- Subtract the thing
-
 main :: IO ()
 main = do
-  let initialState = GameState { playerHp = 100, monsterHp = 80, fstRound = True }
-  -- join (afterFight <$> fightLoop initialState)
-  putStrLn . renderFightResult =<< fightLoop initialState
+  putStrLn . renderFightResult =<< fightLoop initialGameState
 
-data GameState = GameState { playerHp :: Int, monsterHp :: Int, fstRound :: Bool } deriving (Show)
+data GameState = GameState { player :: Player, monster :: Monster, fstRound :: Bool } deriving (Show)
 
 data FightResult = Win | Died | Fighting
 
 data PlayerCommand = Attack | Flee
+
+-- Monster have stat block - which is the statistics of a monster.
+    -- Type
+    -- Armor Class
+    -- Attributes
+    -- Skills
+    -- Hp
+    -- Challenge (amount of exp and how hard is the monster)
+
+data Monster = Monster { monsterHp :: Int, monsterArmorClass :: Int } deriving (Show)
+
+data Player = Player { playerHp :: Int, playerArmorClass :: Int } deriving (Show)
+
+initialGameState :: GameState
+initialGameState = GameState { player = Player { playerHp = 100, playerArmorClass = 12 }, monster = Monster { monsterHp = 80, monsterArmorClass = 8 }, fstRound = True }
+
+-- Fight Rendering
 
 parsePlayerCommand :: Text -> Maybe PlayerCommand
 parsePlayerCommand command =
@@ -39,14 +49,6 @@ getPlayerCommand :: IO Text
 getPlayerCommand =
   T.pack <$> getLine
 
-monsterHealth :: GameState -> Int
-monsterHealth =
-  monsterHp
-
-playerHealth :: GameState -> Int
-playerHealth =
-  playerHp
-
 roundMessage :: Bool -> String
 roundMessage fstRound =
   if fstRound then
@@ -55,65 +57,43 @@ roundMessage fstRound =
     "The Rat is still alive! You are weak... Should you (A)ttack or (F)lee?"
 
 fightStep :: GameState -> IO (GameState, FightResult)
-fightStep g@GameState { playerHp, monsterHp, fstRound } =
+fightStep g@GameState { player, monster, fstRound } =
   putStrLn (roundMessage fstRound) *>
   do
-    -- putStrLn . roundMessage $ fstRound -> I could move line above into here
     mPlayerCommand <- parsePlayerCommand <$> getPlayerCommand
     case mPlayerCommand of
       Just Attack -> do
-        -- Printing on Screen
+        -- Generating random numbers (rolls, attacks)
+        playerAttackRoll <- rollDie D20
+        monsterAttackRoll <- rollDie D20
+        playerAttackDamage <- damageRoll (attackRoll (monsterArmorClass monster) playerAttackRoll)
+        monsterAttackDamage <- damageRoll (attackRoll (playerArmorClass player) monsterAttackRoll)
 
-        -- Generating random things
-        playerAttackDamage <- rollDie D4
-        monsterAttackDamage <- rollDie D8
-
-        let monsterHp' = monsterHp - playerAttackDamage
-        let playerHp' = playerHp - monsterAttackDamage
+        let monsterHp' = monsterHp monster - playerAttackDamage
+        let playerHp' = playerHp player - monsterAttackDamage
 
         -- Printing
         putStrLn ("You hit " ++ show playerAttackDamage ++ "!")
         putStrLn ("Rat hit you " ++ show monsterAttackDamage ++ "!")
-        putStrLn ("Giant Rat HP: " ++ show monsterHp' ++ " VS. " ++ "Player HP: " ++ show playerHp')
+        putStrLn ("Giant Rat HP: " ++ show monsterHp' ++ " VS. " ++ "Player HP: " ++ show playerHp' ++ "\n")
 
-        -- Refactor this out into a new function
-        pure $ if monsterHp' > 0 && playerHp' > 0 then
-          (g { monsterHp = monsterHp', playerHp = playerHp', fstRound = False }, Fighting)
-        else if monsterHp' <= 0 && playerHp' > 0 then
-          (g { monsterHp = monsterHp', fstRound = False }, Win)
-        else
-          (g, Died)
+        pure $ someFnName g monsterHp' playerHp'
       Just Flee -> do
         putStrLn "Coward. While running you steped into a rock, fell, banged your head, and died."
-        pure (g { playerHp = 0, fstRound = False }, Died)
+        pure (g { player = player {playerHp = 0} , fstRound = False }, Died)
       Nothing -> do
         putStrLn "What is this non-sense?"
         fightStep g
 
 
-data DiceType = D4 | D8 | D20
+someFnName :: GameState -> Int -> Int -> (GameState, FightResult)
+someFnName g monsterHp playerHp
+  | monsterHp > 0 && playerHp > 0 =
+    (g { monster = (monster g) { monsterHp = monsterHp }, player = (player g) { playerHp = playerHp }, fstRound = False }, Fighting)
+  | monsterHp <= 0 && playerHp > 0 =
+    (g { monster = (monster g) { monsterHp = monsterHp }, fstRound = False }, Win)
+  | otherwise = (g, Died)
 
-rollDie :: DiceType -> IO Int
-rollDie = \case
-  D4 ->
-    diceRoll 4
-  D8 ->
-    diceRoll 8
-  D20 ->
-    diceRoll 20
-  where
-    diceRoll maxRoll = uniformRM (1, maxRoll) globalStdGen
-
-
--- <$> :: (a -> b) -> k a -> k b
---        (DiceType -> IO Int) -> NonEmpty DiceType -> NonEmpty (IO Int)
-rollDice :: NonEmpty DiceType -> IO (NonEmpty Int)
-rollDice = traverse rollDie
--- rollDice d = sequenceA (rollDie <$> d)
-
-
--- Advantange Roll
--- Disadiventage Roll - Take the
 
 fightLoop :: GameState -> IO FightResult
 fightLoop gs = do
@@ -138,34 +118,48 @@ renderFightResult = \case
       "This should not happen. Blame the developer"
 
 
--- USE THIS
+-- Die Roll
 
-attackResult :: IO ()
-attackResult = undefined
+data DiceType = D4 | D8 | D20
 
-data AttackOutcome = CriticalSuccess | CriticalMiss | RegularAttack | Miss
+rollDie :: DiceType -> IO Int
+rollDie = \case
+  D4 ->
+    diceRoll 4
+  D8 ->
+    diceRoll 8
+  D20 ->
+    diceRoll 20
+  where
+    diceRoll maxRoll = uniformRM (1, maxRoll) globalStdGen
+
+
+rollDice :: NonEmpty DiceType -> IO (NonEmpty Int)
+rollDice = traverse rollDie
+
+-- Advantange Roll - Take the highest roll
+-- Disadvantage Roll - Take the lowest roll
+
+-- Attack
+
+damageRoll :: AttackOutcome -> IO Int
+damageRoll = \case
+  CriticalSuccess ->
+    -- We are rolling D20, normally it rolls the weapon damage.
+    rollDie D20
+  RegularAttack ->
+    rollDie D20
+  Miss ->
+    pure 0
+  CriticalFailure ->
+    pure 0
+
+data AttackOutcome = CriticalSuccess | CriticalFailure | RegularAttack | Miss
 
 attackRoll :: Int -> Int -> AttackOutcome
-attackRoll monsterAc roll
+attackRoll targetAc roll
   | roll == 20 = CriticalSuccess
-  | roll == 1 = CriticalMiss
-  | roll >= monsterAc = RegularAttack
+  | roll == 1 = CriticalFailure
+  | roll >= targetAc = RegularAttack
   | otherwise = Miss
-
--- IO FightResult -> FightResult -> IO ()
-{-
-fightLoop' gs =
-  fightStep gs >>= \(gs', fr) ->
-  case fr of
-    ...
--}
-
-
-
-
-
-
-
--- List Comprehension might be useful
--- [ x+y | (x,y) <- List of Tuples - [(1,2)] ]
 
