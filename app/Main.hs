a846009e55c6d8998d2b4e1853d0049af6154b74
Gustavo Carvalho <gustavo.hcarvalho@outlook.com>
2023-10-26 11:16:32 -0300

    feat: uses State Monad in our game state updates

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings, NamedFieldPuns, LambdaCase #-}
+{-# LANGUAGE OverloadedStrings, NamedFieldPuns, LambdaCase, InstanceSigs, RecordWildCards #-}
 
 module Main where
 
@@ -18,6 +18,12 @@ data FightResult = Win | Died | Fighting
 
 data PlayerCommand = Attack | Flee
 
+-- Map
+  -- Generate monster and state of monsters there
+  -- Exploration
+  -- Generate monster based on the challenging rating
+  --
+
 -- Monster have stat block - which is the statistics of a monster.
     -- Type
     -- Armor Class
@@ -27,12 +33,93 @@ data PlayerCommand = Attack | Flee
     -- Attributes
     -- Skills
     -- Challenge (amount of exp and how hard is the monster)
+    -- isLegendary :: Bool
 
 data Monster = Monster { name :: Text, statBlock :: StatBlock } deriving (Show)
 
 data StatBlock = StatBlock { hp :: Int, armorClass :: Int } deriving (Show)
 
 data Player = Player { playerHp :: Int, playerArmorClass :: Int } deriving (Show)
+
+-- --
+newtype State s a =
+  State (
+    s
+    -> (a, s)
+  )
+
+runState ::
+  State s a
+  -> s
+  -> (a, s)
+runState (State f) =
+  f
+
+get ::
+  State s s
+get =
+  State (\a -> (a, a))
+
+put ::
+  s
+  -> State s ()
+put state =
+  State (\_ -> ((), state))
+
+
+instance Functor (State s) where
+  fmap ::
+    (a -> b)
+    -> State s a
+    -> State s b
+  fmap f (State fn) =
+    State $ \s0 ->
+      let (a, s1) = fn s0
+          b = f a
+      in
+      (b, s1)
+
+instance Applicative (State s) where
+  pure ::
+    a
+    -> State s a
+  pure a =
+    State $ \s -> (a, s)
+  (<*>) ::
+    State s (a -> b)
+    -> State s a
+    -> State s b
+  (<*>) (State sab) (State sa)  =
+    State $ \s0 -> let
+      (a2b, s1) = sab s0
+      (a, s2) = sa s1
+      in
+      (a2b a, s2)
+
+
+instance Monad (State s) where
+  (>>=) ::
+    State s a
+    -> (a -> State s b)
+    -> State s b
+  (>>=) (State sa) assb =
+    State $ \s0 -> let
+      (a, s1) = sa s0
+      ssb = assb a
+      in
+      runState ssb s1
+
+-- incrementNum :: State Int Int
+-- incrementNum = do
+--   x <- get
+--   put $ x + 1
+--   y <- get
+--   pure y
+
+-- runIncrementNum :: (Int, Int)
+-- runIncrementNum =
+--   runState incrementNum 20
+-- --
 
 initialGameState :: GameState
 initialGameState = GameState {
@@ -74,7 +161,7 @@ monsterAc :: Monster -> Int
 monsterAc monster =
   armorClass (statBlock monster)
 
-fightStep :: GameState -> IO (GameState, FightResult)
+fightStep :: GameState -> IO (FightResult, GameState) -- StateT
 fightStep g@GameState { player, monster, fstRound } =
   putStrLn (roundMessage (name monster) fstRound) *>
   do
@@ -95,10 +182,16 @@ fightStep g@GameState { player, monster, fstRound } =
         putStrLn ("Rat hit you " ++ show monsterDamageRoll ++ "!")
         putStrLn ("Giant Rat HP: " ++ show monsterHp' ++ " VS. " ++ "Player HP: " ++ show playerHp' ++ "\n")
 
-        pure $ computeBattleState g monsterHp' playerHp'
+        pure $ runState (computeBattleState' monsterHp' playerHp') g
+        -- IO (State GameState FightResult)
       Just Flee -> do
         putStrLn "Coward. While running you steped into a rock, fell, banged your head, and died."
-        pure (g { player = player {playerHp = 0} , fstRound = False }, Died)
+        pure $ flip runState g $ do
+          GameState {..} <- get
+          let updateState = GameState { player = player {playerHp = 0} , fstRound = False, ..}
+          put updateState
+          pure Died
+
       Nothing -> do
         putStrLn "What is this non-sense?"
         fightStep g
@@ -112,9 +205,24 @@ computeBattleState g@GameState { player, monster } monsterHp playerHp
     (g { monster =  monster { statBlock = (statBlock monster) { hp = monsterHp } }, fstRound = False }, Win)
   | otherwise = (g, Died)
 
+                                  -- State s a -> (s, a)
+computeBattleState' :: Int -> Int -> State GameState FightResult
+computeBattleState' monsterHp playerHp
+  | monsterHp > 0 && playerHp > 0 = do
+      GameState {..} <- get
+      let updatedState = GameState { monster = monster { statBlock = (statBlock monster) { hp = monsterHp } }, player = player { playerHp = playerHp }, fstRound = False, ..}
+      put updatedState
+      pure Fighting -- WHERE
+  | monsterHp <= 0 && playerHp > 0 = do
+      GameState {..} <- get
+      let updateState = GameState { monster =  monster { statBlock = (statBlock monster) { hp = monsterHp } }, fstRound = False, ..}
+      put updateState
+      pure Win
+  | otherwise = pure Died
+
 fightLoop :: GameState -> IO FightResult
 fightLoop gs = do
-  (gs', fr) <- fightStep gs
+  (fr, gs') <- fightStep gs
   case fr of
     Win ->
       pure fr
@@ -148,7 +256,6 @@ rollDie = \case
   where
     diceRoll maxRoll = uniformRM (1, maxRoll) globalStdGen
 
-
 rollDice :: NonEmpty DiceType -> IO (NonEmpty Int)
 rollDice = traverse rollDie
 
@@ -160,7 +267,7 @@ rollDice = traverse rollDie
 damageRoll :: AttackOutcome -> IO Int
 damageRoll = \case
   CriticalSuccess ->
-    -- We are rolling D20, normally it rolls the weapon damage.
+    -- We are rolling D20, normally it rolls the weapon damage
     rollDie D20
   RegularAttack ->
     rollDie D20
